b7cfee4a4d20deedacdfac18b7216be216936c00
abhishekupadhyaycs28-wq <abhishek.upadhyay.cs28@iilm.edu>
2026-03-18 20:55:18 +0530

    Create smallest_subarray_with_sum_greater_than_x.py

---
+++ smallest_subarray_with_sum_greater_than_x.py
@@ -0,0 +1,18 @@
+class Solution:
+    def smallestSubWithSum(self, x, arr):
+        n = len(arr)
+        min_len = n + 1
+        start = 0
+        end = 0
+        curr_sum = 0
+        
+        while end < n:
+            while curr_sum <= x and end < n:
+                curr_sum += arr[end]
+                end += 1
+            while curr_sum > x and start < n:
+                if end - start < min_len:
+                    min_len = end - start
+                curr_sum -= arr[start]
+                start += 1
+        return 0 if min_len == n + 1 else min_len
